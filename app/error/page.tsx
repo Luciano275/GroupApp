ae002d6d95e786899e9ed9d3b0fc5ddcea26ec33
Luciano275 <luna800153@gmail.com>
2024-03-26 22:37:25 -0300

    🚀 Create error page for next auth

---
+++ app/error/page.tsx
@@ -0,0 +1,8 @@
+export default function AuthErrorPage() {
+  return (
+    <main className="p-4">
+      <h2 className="text-center py-4 px-2 text-3xl">Algo salió mal ☹️</h2>
+      <button className="w-fit py-2 px-4 text-xl rounded bg-violet-600 mx-auto mt-5">Volver al inicio</button>
+    </main>
+  )
+}
