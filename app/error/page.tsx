329adfe00c24881a9985eec95cfe39903d29eb20
Luciano Luna <71286132+Luciano275@users.noreply.github.com>
2024-03-28 00:08:04 +0000

    Center the button

--- app/error/page.tsx
+++ app/error/page.tsx
@@ -1,8 +1,12 @@
+import Link from "next/link";
+
 export default function AuthErrorPage() {
   return (
     <main className="p-4">
       <h2 className="text-center py-4 px-2 text-3xl">Algo salió mal ☹️</h2>
-      <button className="w-fit py-2 px-4 text-xl rounded bg-violet-600 mx-auto mt-5">Volver al inicio</button>
+      <div className="flex justify-center">
+        <Link href="/" className="w-fit py-2 px-4 text-xl rounded bg-violet-600 mx-auto mt-5 hover:bg-fuchsia-800">Volver al inicio</Link>
+      </div>
     </main>
   )
 }
